8b7bda93f14b97496315ec8d65f9e34f6c7a39e5
LycorisOni <165130248+LycorisOni@users.noreply.github.com>
2025-10-20 16:31:53 -0400

    Update OniTrader.cs
    Should be a good last check for 4.0

--- OniTrader.cs
+++ OniTrader.cs
@@ -209,21 +209,21 @@ public class EditDatabaseValues(
         {
             var filesToCheck = new List<FileCheckerData>();
 
-            var AssortFileChecksum = new FileCheckerData
+            var assortFileChecksum = new FileCheckerData
             {
                 RelativePath = "data/assort.json",
                 ExpectedHash = "b7ad2b4bf069fe9041fa6424d2aaa432",
                 Name = "assort.json"
             };
-            filesToCheck.Add(AssortFileChecksum);
+            filesToCheck.Add(assortFileChecksum);
 
-            var BaseFileChecksum = new FileCheckerData
+            var baseFileChecksum = new FileCheckerData
             {
                 RelativePath = "data/base.json",
                 ExpectedHash = "520f5c5170ee5eb6489ec1d5f75c8371",
                 Name = "base.json"
             };
-            filesToCheck.Add(BaseFileChecksum);
+            filesToCheck.Add(baseFileChecksum);
 
             var failedFiles = new List<string>();
 
